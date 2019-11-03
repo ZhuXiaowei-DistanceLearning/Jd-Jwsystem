88e509004b293c59214247a9dd2d451cd7ba1b14
zxw <502513206@qq.com>
2019-11-03 17:28:16 +0800

    first

---
+++ src/main/java/com/zxw/JwSystemApplication.java
@@ -0,0 +1,15 @@
+package com.zxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxw
+ * @date 2019/11/3 17:25
+ */
+@SpringBootApplication
+public class JwSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JwSystemApplication.class);
+    }
+}
