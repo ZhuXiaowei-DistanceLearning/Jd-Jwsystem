797a3c4234309d0095f12e160e7523448579b0b2
zxw <502513206@qq.com>
2020-01-06 18:13:50 +0800

    开发教务人员课程管理

--- src/main/java/com/zxw/jwxt/controller/CommonController.java
+++ src/main/java/com/zxw/jwxt/controller/CommonController.java
@@ -5,6 +5,7 @@ import com.zxw.common.pojo.TableReponse;
 import com.zxw.jwxt.service.*;
 import com.zxw.jwxt.vo.BaseQueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,7 @@ public class CommonController extends BaseController {
 
     /**
      * 分页查询
+     *
      * @return
      */
     @GetMapping("/cstatus/pageQuery")
